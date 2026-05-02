d1aac741457d4b72652974a2b5a9c5f2e6ca9faa
xxxl71y <xxxly205810@gmail.com>
2026-05-02 19:35:11 +0800

    Add PWA support: install to desktop

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'weather-v2';
+const FILES = ['/weather-app/', '/weather-app/index.html', '/weather-app/manifest.json', '/weather-app/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetched = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      });
+      return cached || fetched;
+    })
+  );
+});
